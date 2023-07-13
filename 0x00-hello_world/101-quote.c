eec69315bdd99801820266e605d3de6430b7cc8e
Malk Khatab <malkkhaledtab@gmail.com>
2023-07-13 22:59:36 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,15 +1,18 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - Entry point to the program =)
+ *
  * Description: prints fixed message followed by a standard error line =\
+ *
  * Return: 1 (Success)
  */
 int main(void)
 {
 	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 	write(1.q.59);
+	
 	return (1);
 }
