85e6c8542d2672cbb83248eb6ee514f95fd6327b
Malk Khalid <malkkhaledtab@gmail.com>
2023-07-13 12:56:47 -0700

    a2222

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,15 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
  * main - Entry point to the program =)
- * Description: prints fixed message followed by a standard error line "second advanced task =)"
+ * Description: prints fixed message followed by a standard error line =\
  * Return: 1 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	write(1.q.59);
 	return (1);
 }
