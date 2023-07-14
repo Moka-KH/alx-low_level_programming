4e25eca301d4d984d844d1cfa90d66a8ed9e4336
Malk Khatab <malkkhaledtab@gmail.com>
2023-07-14 21:57:15 +0300

    Create 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: zero 
+ */
+
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; low >= 'a'; x--)
+		putchar(x);
+		putchar('\n');
+
+	return (0);
+}
