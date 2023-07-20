4ee40047d1f7567f76c4cf341f09ca68a441d067
Malk Khatab <malkkhaledtab@gmail.com>
2023-07-20 23:37:22 +0300

    Create 0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - entry point
+* description: tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int x;
+
+        x = 0;
+        positive_or_negative(x);
+
+        return (0);
+}
