aecf81a3e1f925ae2d8f9fed96ae4b9dbeb35e3e
Malk Khatab <malkkhaledtab@gmail.com>
2023-08-24 20:55:02 +0300

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+/**
+ * _puts_recursion - a function that prints a string
+ * @s: input string
+ * Return: Nothing
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
