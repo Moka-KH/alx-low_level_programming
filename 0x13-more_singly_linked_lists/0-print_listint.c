845f9faca4d02b73ae3c54365d0989a21ac44aee
Malk Khatab <malkkhaledtab@gmail.com>
2023-08-29 21:29:36 +0300

    Update t0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,13 +7,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t number_of_nodes = 0;
+	size_t nodesNum = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h -> n);
 		h = h -> next;
-		number_of_nodes++;
+		nodesNum++;
 	}
-	return (number_of_nodes);
+	return (nodesNum);
 }
