850fd66364721655dd6d5388badae28252d9f712
Malk Khatab <malkkhaledtab@gmail.com>
2023-08-29 23:00:21 +0300

    Update t0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list
+ * print_listint - prints all the 
+ * elements of a list
  * @h: head of list
  *
  * Return: num of nodes
