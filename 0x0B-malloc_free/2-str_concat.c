d55dd782bf5ed96d7187e4095ad8c4aecd5b134f
Malk Khatab <malkkhaledtab@gmail.com>
2023-08-30 22:30:44 +0300

    t2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,19 +1,19 @@
-#include "holberton.h"
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * str_concat - A function that concatenates two strings
- * @s1: An input pointer of the first string
- * @s2: An input pointer of the second string
- * Return: Apointer to concatened strings or NULL if it str is NULL
+ * str_concat - concatenates two strings
+ * @s1: pointer of the 1st string
+ * @s2: pointer of the 2nd string
+ * Return: pointer to new string or NULL if the string is NULL
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *new_str, *starts1, *starts2;
+	char *new_str, *first, *second;
 	int i = 0, lens1 = 0, lens2 = 0;
 
-	starts1 = s1;
-	starts2 = s2;
+	first = s1;
+	second = s2;
 	if (s1 == NULL)
 		s1 = "";
 	while (*s1)
@@ -21,7 +21,7 @@ char *str_concat(char *s1, char *s2)
 		lens1++;
 		s1++;
 	}
-	s1 = starts1;
+	s1 = first;
 	if (s2 == NULL)
 		s2 = "";
 	while (*s2)
@@ -29,9 +29,9 @@ char *str_concat(char *s1, char *s2)
 		lens2++;
 		s2++;
 	}
-	s2 = starts2;
+	s2 = second;
 	new_str = malloc(sizeof(char) * (lens1 + lens2 + 1));
-	starts1 = new_str;
+	first = new_str;
 	if (new_str == NULL)
 		return (NULL);
 	for (; i < (lens1 + lens2); i++)
@@ -48,5 +48,5 @@ char *str_concat(char *s1, char *s2)
 		}
 	}
 	new_str[i] = '\0';
-	return (starts1);
+	return (first);
 }
