a14622537bdc8da95328095ade74a18b1a10f123
Malk Khatab <malkkhaledtab@gmail.com>
2023-08-30 22:26:30 +0300

    t0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	chr = malloc(sizeof(c) * size);
+	ch = malloc(sizeof(c) * size);
 
 	if (ch == NULL)
 		return (NULL);
