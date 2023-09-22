aebc7f6b303d93b8ae2385fb8a63ead06f727758
Malk Khatab <malkkhaledtab@gmail.com>
2023-09-22 16:36:32 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,42 @@
 #include "main.h"
 
 /**
- * create_file - create a file
- * @filename: file name 
- * @text_content: string to write to the file
+ * _strlen - returns the length of a string
+ * @s: string 
  *
- * Return: 1 on success, -1 on failure
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content: text to write
+ *
+ * Return: 1 on success 0 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int to_be_printed;
-	int read_write;
+	ssize_t bytesNum = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (to_be_printed = 0; text_content[to_be_printed]; to_be_printed++);
-
-
-	read_write = write(fd, text_content, to_be_printed);
-
-	if (read_write == -1)
-		return (-1);
-
+	if (len)
+		bytesNum = write(fd, text_content, len);
 	close(fd);
-
-	return (1);
+	return (bytesNum == len ? 1 : -1);
 }
